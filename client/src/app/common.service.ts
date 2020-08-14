645cdeaf336a765842c1950ac937023e4914249e
CaptWalker <28630298+CaptWalker@user.noreply.github.com>
2020-08-14 15:10:47 +0530

    minor tweak

--- client/src/app/common.service.ts
+++ client/src/app/common.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { PatientRegistration } from './model/patientRegistration';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,35 @@ import { PatientRegistration } from './model/patientRegistration';
 export class CommonService {
 
   constructor(private http: HttpClient) { }
-  url = 'http://localhost:8080';
 
-  // getLoginForPatient = (loginCred): Observable<PatientRegistration> => {
-  //   return this.http.post( this.url + '/patientregistration/login', loginCred).pipe(
-  //     .
-  //   )
+  // url = 'https://covid-backend-hw-dev-covid-hw.sandbox-ocp431-one-89dadfe96916fcd27b299431d0240c37-0000.eu-gb.containers.appdomain.cloud/getpatient';
+
+  // header = new HttpHeaders()
+  // .set('Access-Control-Allow-Origin', '*')
+  // .set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT');
+
+  // getPatient = (): Observable<any> => {
+  //   return this.http.get<PatientRegistration>(this.url, {headers: this.header}).pipe(
+  //     map((response: any) => {
+  //       console.log(response);
+  //       return response;
+  //     }),
+  //     catchError(this.handleError)
+  //   );
+  // }
+
+  // private handleError(err: HttpErrorResponse) {
+  //   let errMsg = '';
+  //   if (err.error instanceof Error) {
+  //     // A client-side or network error occurred. Handle it accordingly.
+  //     console.log('An error occurred:', err.error.message);
+  //     errMsg = err.error.message;
+  //   } else {
+  //     // The backend returned an unsuccessful response code.
+  //     // The response body may contain clues as to what went wrong,
+  //     console.log(`Backend returned code ${err.status}`);
+  //     errMsg = err.error.status;
+  //   }
+  //   return throwError(errMsg);
   // }
 }
--- client/src/app/patient-physical-health/patient-physical-health.component.ts
+++ client/src/app/patient-physical-health/patient-physical-health.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, FormArray } from '@angular/forms';
+import { CommonService } from '../common.service';
 
 @Component({
   selector: 'app-patient-physical-health',
@@ -9,179 +10,12 @@ import { FormGroup, FormBuilder, FormControl, FormArray } from '@angular/forms';
 export class PatientPhysicalHealthComponent implements OnInit {
 
   physicalAndHealthForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private service: CommonService) { }
 
   symptomStatus = false;
   isDisabled = false;
   medicalInsuranceStatus = false;
   agreementStatus = false;
-
-  // symptomsList = [
-  //   {
-  //     id: 1,
-  //     symptom: 'Cough',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 2,
-  //     symptom: 'Diarrhoea',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 3,
-  //     symptom: 'Vomitting',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 4,
-  //     symptom: 'Fever',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 5,
-  //     symptom: 'Body Ache',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 6,
-  //     symptom: 'Abdominal Pain',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 7,
-  //     symptom: 'Breathlessness',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 8,
-  //     symptom: 'Nausea',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 9,
-  //     symptom: 'Haemoptysis',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 10,
-  //     symptom: 'Sore Throat',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 11,
-  //     symptom: 'Chest pain',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 12,
-  //     symptom: 'Nasal discharge',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 13,
-  //     symptom: 'Sputum',
-  //     checked: false
-  //   }
-  // ];
-
-  // existingDiseaseList = [
-  //   {
-  //     id: 1,
-  //     disease: 'Blood Pressure',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 2,
-  //     disease: 'Diabetes',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 3,
-  //     disease: 'Thyroid',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 4,
-  //     disease: 'Asthma',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 5,
-  //     disease: 'Pneumonia',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 6,
-  //     disease: 'Chronic Lung Disease',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 7,
-  //     disease: 'Malignancy',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 8,
-  //     disease: 'Heart Disease',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 9,
-  //     disease: 'Chronic liver disease',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 10,
-  //     disease: 'Hypertension',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 11,
-  //     disease: 'Chronic renal disease',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 12,
-  //     disease: 'Other',
-  //     checked: false
-  //   }
-  // ];
-
-  // medicalHistoryList = [
-  //   {
-  //     id: 1,
-  //     history: 'Bone Fracture',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 2,
-  //     history: 'Fatal Accident',
-  //     checked: false
-  //   }
-  // ];
-
-  // habitsList = [
-  //   {
-  //     id: 1,
-  //     habit: 'Eating Junkfood',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 2,
-  //     habit: 'Occasional Drinker',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 3,
-  //     habit: 'Chronic Drinker',
-  //     checked: false
-  //   },
-  //   {
-  //     id: 4,
-  //     habit: 'Smoker',
-  //     checked: false
-  //   },
-  // ];
 
   ngOnInit(): void {
     this.physicalAndHealthForm = this.formBuilder.group({
@@ -210,7 +44,15 @@ export class PatientPhysicalHealthComponent implements OnInit {
       medicalInsurance: [],
       doctorDetailDTO: []
     });
+    //this.getPatient();
+
   }
+
+  // getPatient = () => {
+  //   this.service.getPatient().subscribe((response)=>{
+  //     console.log(response);
+  //   });
+  // }
 
   initTravel = () => {
     return this.formBuilder.group({
@@ -238,26 +80,6 @@ export class PatientPhysicalHealthComponent implements OnInit {
     data.doctorDetailDTO = { doctorId: null };
     console.log(data);
   }
-
-  // onSymptomsChange = (id) => {
-  //   this.symptomsList[id - 1].checked = !this.symptomsList[id - 1].checked;
-  // }
-
-  // onExistingDiseaseChange = (id) => {
-  //   this.existingDiseaseList[id - 1].checked = !this.existingDiseaseList[id - 1].checked;
-  // }
-
-  // onExistingDiseaseChangeNone = () => {
-  //   this.isDisabled = !this.isDisabled;
-  // }
-
-  // onMedicalHistoryChange = (id) => {
-  //   this.medicalHistoryList[id - 1].checked = !this.medicalHistoryList[id - 1].checked;
-  // }
-
-  // onHabitsChange = (id) => {
-  //   this.habitsList[id - 1].checked = !this.habitsList[id - 1].checked;
-  // }
 
   symptomStatusToggle = () => {
     this.symptomStatus = !this.symptomStatus;
